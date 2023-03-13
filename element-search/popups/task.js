cbcd1666e0b25f2e524de7e2a793e0d873c0f183
AlekseyNikol <faithinlove5.an@gmail.com>
2023-03-13 18:42:20 +0300

    popups

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,6 +1,31 @@
-let main = document.getElementById('modal_main')
+let main = document.getElementById('modal_main');
+//onsole.log(main)
+
+let mainSuccess = document.getElementById('modal_success');
+//console.log(mainSuccess)
+
+let modalClose = document.querySelectorAll('.modal__close_times')
+//console.log(modalClose)
+
+let btnDanger = document.querySelector('.btn_danger')
+//console.log(btnDanger)
 
 main.classList.add('modal_active') //добавляем
+
+modalClose.forEach(
+    function (item) {
+        item.onclick = () => {
+            main.classList.remove('modal_active')
+            mainSuccess.classList.remove('modal_active')
+        }
+    }
+) // крестик
+
+
+btnDanger.onclick = () => {
+    main.classList.remove('modal_active');
+    mainSuccess.classList.add('modal_active');
+} // кнопка
 
 
 
