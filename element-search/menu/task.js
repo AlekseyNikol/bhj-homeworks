59c8af75d84f95aff422bc41e087e287db2c7e79
AlekseyNikol <faithinlove5.an@gmail.com>
2023-03-14 18:56:36 +0300

    menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -5,20 +5,6 @@ const MenuItem = document.querySelectorAll('.menu__item')
 const MenuSub = document.querySelectorAll('.menu_sub')
 //console.log(MenuSub)
 
-const MenuMain = document.querySelectorAll('.menu_main')
-//console.log(MenuMain)
-
-const MenuLink = document.querySelectorAll('.menu__link')
-//console.log(MenuLink)
-
-//добавляем
-/*
-for (let item of MenuSub) {
-   item.classList.add('menu_active')
-}
-*/
-//MenuSub.classList.add('menu_active') // так не добавляется ко всем элементам?
-
 //функция меню
 
 MenuItem.forEach((item) => {
