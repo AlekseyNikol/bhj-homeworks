e765ca7238fc3242603aab5856f36bea069daa4f
AlekseyNikol <faithinlove5.an@gmail.com>
2023-03-16 13:41:24 +0300

    prev finish

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -13,28 +13,28 @@ console.log(arrayItm)
 
 function prev() {
    //let arraySlider = sliderItems.findIndex(e => e == "slider__item_active"); // почему не срабатывает метод findIndex в данном случае?
-   let arraySlider = arrayItm.findIndex(e => e.classList.contains("slider__item_active")); // зачем проверять наличие CSS класса элемента? без мотода contains возвращает (-1). И почему строка не работает без Array (arrayItm), то есть с переменной arrowNext?
-   //console.log(arraySlider)
-   arrayItm[arraySlider].classList.remove("slider__item_active");
+   let sliderIndex = arrayItm.findIndex(e => e.classList.contains("slider__item_active")); // зачем проверять наличие CSS класса элемента? без мотода contains возвращает (-1). И почему строка не работает без Array (arrayItm), то есть с переменной arrowNext?
+
+   arrayItm[sliderIndex].classList.remove("slider__item_active");
    console.log(arrayItm)
-   if (arraySlider > 0) {
-      arraySlider -= 1
+   if (sliderIndex > 0) {
+      sliderIndex -= 1
    } else {
-      arraySlider = arrayItm.length - 1; //почему в условии(-1), зачем мы из длины массива вычетаем один элемент?
+      sliderIndex = arrayItm.length - 1; //почему в условии(-1), зачем мы из длины массива вычетаем один элемент?
    }
-   return arrayItm[arraySlider].classList.add("slider__item_active")
+   return arrayItm[sliderIndex].classList.add("slider__item_active")
 }
 
 function next() {
-   let arraySlider = arrayItm.findIndex(e => e.classList.contains('slider__item_active'));
-   arrayItm[arraySlider].classList.remove('slider__item_active');
+   let sliderIndex = arrayItm.findIndex(e => e.classList.contains('slider__item_active'));
+   arrayItm[sliderIndex].classList.remove('slider__item_active');
 
-   if (arraySlider < arrayItm.length - 1) {
-      arraySlider += 1;
+   if (sliderIndex < arrayItm.length - 1) {
+      sliderIndex += 1;
    } else {
-      arraySlider = 0;
+      sliderIndex = 0;
    }
-   return arrayItm[arraySlider].classList.add("slider__item_active")
+   return arrayItm[sliderIndex].classList.add("slider__item_active")
 }
 
 arrowPrev.addEventListener('click', prev) // стрелка назад
