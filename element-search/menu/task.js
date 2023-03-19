22fca1c05e19075e97902bbfa6dc94ddeb3d06dd
AlekseyNikol <faithinlove5.an@gmail.com>
2023-03-19 22:24:03 +0300

    camelCase for constanta

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,28 +1,40 @@
 //переменные
-const MenuItem = document.querySelectorAll('.menu__item')
+const menuItem = document.querySelectorAll('.menu__item')
 //console.log(MenuItem)
 
-const MenuSub = document.querySelectorAll('.menu_sub')
+const menuSub = document.querySelectorAll('.menu_sub')
 //console.log(MenuSub)
 
 //функция меню
 
 MenuItem.forEach((item) => {
    let subMenu = item.querySelector('.menu_sub')
+   console.log(subMenu)
    if (subMenu) {
       let link = item.querySelector('.menu__link')
+      console.log(link)
 
       link.onclick = (e) => {
          e.preventDefault()
          MenuSub.forEach((sub) => {
-            if (sub !== subMenu) {
+            console.log(sub)
+            if (sub !== subMenu) { // почему? ведь пер subMenu("ul.menu.menu_sub") и элемент объекта MenuSub - это (ul.menu.menu_sub)
                sub.classList.remove('menu_active')
             }
          })
-         subMenu.classList.add('menu_active')
+
+         // console.log(item.querySelector('.menu_active'))
+         if (subMenu == item.querySelector('.menu_active')) {
+            subMenu.classList.remove('menu_active')
+         } else {
+            subMenu.classList.add('menu_active')
+         }
       }
 
-   }
 
+
+   }
 })
+
+
 
