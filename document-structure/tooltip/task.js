5854ceac0203a6b38312988b43be4811f2b3185f
AlekseyNikol <faithinlove5.an@gmail.com>
2023-05-30 23:08:39 +0300

    1

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,19 @@
+const tooltip = document.createElement('div');
+console.log(tooltip);
+
+tooltip.setAttribute('class', 'tooltip');
+document.body.appendChild(tooltip);
+
+document.body.addEventListener('click', (event) => {
+   let target = event.target;
+   if (target.className !== 'has-tooltip') return;
+   event.preventDefault();
+   tooltip.innerText = target.getAttribute('title');
+   tooltip.style.left = target.getBoundingClientRect().left.toString() + 'px';
+   tooltip.style.top = (target.getBoundingClientRect().top + 20).toString() + 'px';
+   tooltip.classList.toggle('tooltip_active');
+
+   target.addEventListener('blur', () => {
+      tooltip.classList.remove('tooltip_active');
+   });
+});
