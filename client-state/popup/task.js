5112da05d3ed8bc2a7d0bffc66b36e7e4f7768c4
AlekseyNikol <faithinlove5.an@gmail.com>
2023-08-19 17:56:33 +0300

    second

---
+++ client-state/popup/task.js
@@ -0,0 +1,25 @@
+const modalClose = document.querySelector(".modal__close");
+const modal = document.getElementById("subscribe-modal");
+
+
+function getCookie(name) {
+    const value = "; " + document.cookie;
+    let parts = value.split("; " + name + "=");
+    console.log(parts);
+    //console.log(parts.length);
+    if (parts.length === 2) {
+        return parts
+            .pop()
+            .split(";")
+            .shift();
+    }
+}
+
+if (getCookie('modalclose') !== 'closed') {
+    modal.classList.add("modal_active")
+}
+
+modalClose.onclick = () => {
+    modal.classList.remove("modal_active");
+    document.cookie = 'modalclose=closed';
+}
