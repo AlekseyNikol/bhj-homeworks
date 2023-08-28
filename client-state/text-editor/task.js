74eb738c741e78823318d93d288be3be52e00e37
AlekseyNikol <faithinlove5.an@gmail.com>
2023-08-28 21:39:48 +0300

    finish

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,14 +1,27 @@
 // Задание один
 
 const textarea = document.getElementById("editor");
-const clearBtn = document.getElementById("clear-btn")
+const clearBtn = document.getElementById("btn_clear")
 
 console.log(textarea);
 console.log(clearBtn);
 
 textarea.value = localStorage.text;
+
 textarea.oninput = () => localStorage.text = textarea.value;
-clearBtn.onclick = () => {
-    textarea.value = "";
-    localStorage.text = textarea.value;
-}
+
+clearBtn.addEventListener("click", () => {textarea.value = "";
+    localStorage.text = textarea.value})
+/*
+// one
+
+clearBtn.addEventListener("click", () => {textarea.value = "";
+    localStorage.removeItem("text")}) 
+
+// two
+
+clearBtn.addEventListener("click", () => {textarea.value = "";
+    localStorage.clear()})
+
+---------- почему первый и второй вариант после удаления localStorage в окне textarea выдает undefined?
+*/  
